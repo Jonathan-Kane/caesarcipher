87a8e7459948d484100db744cf2a8443d280fa2c
GenghisKhane <55930491+GenghisKhane@users.noreply.github.com>
2022-02-05 11:11:08 -0600

    Create project.js

---
+++ project.js
@@ -0,0 +1,17 @@
+function rot13(str) {
+  const alpha1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alpha2 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+  let newString = "";
+  for (let i = 0; i < str.length; i++) {
+    let index = alpha1.indexOf(str[i]);
+
+    if (/[a-z]/i.test(str[i])) {
+      newString += alpha2[index];
+    } else {
+      newString += str[i];
+    }
+
+  }
+  return newString;
+}
+rot13("SERR PBQR PNZC");
